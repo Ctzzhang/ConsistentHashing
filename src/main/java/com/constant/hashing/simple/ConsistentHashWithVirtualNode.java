c14b5c9b236daf6f19ac27251697df434bf712a2
Ctzzhang <tzzhang_021@163.com>
2019-07-16 22:50:31 +0800

    main

---
+++ src/main/java/com/constant/hashing/simple/ConsistentHashWithVirtualNode.java
@@ -0,0 +1,14 @@
+/**
+ * tzzhang
+ * 下午10:48:05
+ */
+package com.constant.hashing.simple;
+
+/**
+ * 一致性hash带有虚拟节点
+ * @author tzzhang
+ * @version create on 2019年7月16日
+ */
+public class ConsistentHashWithVirtualNode {
+
+}
---
+++ src/main/java/com/constant/hashing/simple/ConstantHashMian.java
@@ -0,0 +1,22 @@
+/**
+ * tzzhang
+ * 下午10:45:59
+ */
+package com.constant.hashing.simple;
+
+/**
+ * 一致性hash测试
+ * @author tzzhang
+ * @version create on 2019年7月16日
+ */
+public class ConstantHashMian {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
